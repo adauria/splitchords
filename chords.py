390eb3463e07eb985f1989f21993dacc873e17a1
adauria <adauria@gmail.com>
2016-08-02 16:10:51 -0400

    second commit

---
+++ chords.py
@@ -0,0 +1,29 @@
+# Chord Progression Analyzer
+# Version 0.1
+# By Andrew D'Auria
+
+# prompt for file name
+fileName = input("Name of text file with chords: ")
+# vaildate filename
+# open file
+
+# prompt for key
+key = input("Key of song /(blank for major, m for minor/): ") 
+# validate key
+# load chord to number matches
+
+print(fileName)
+print(key)
+
+# determine chord notation format
+
+# separate song into parts
+
+# extract chords per part
+
+# match chords to numbers
+
+# write out numbers to file
+
+
+
